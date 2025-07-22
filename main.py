fffc46b7a4901639243bd60e9e6499ffbf22b981
ltkfa <ae86mm@gmail.com>
2025-07-21 23:21:55 -0600

    update

--- main.py
+++ main.py
@@ -11,7 +11,12 @@ def sumar(n):
     else:
         return n+sumar(n-1)
 
+def fibonacci(n):
+    if n <= 1:
+        return n
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)
 
 
-
+def contadordeletras(palabra, letra):
 

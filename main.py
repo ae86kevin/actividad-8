bff3d6b8ecc7b371aeed19e2413ef5b38b1b2a99
ltkfa <ae86mm@gmail.com>
2025-07-21 22:31:00 -0600

    update

---
+++ main.py
@@ -0,0 +1,17 @@
+def factoria(n):
+    if n ==0:
+        return 1
+    else:
+        return n*factoria(n-1)
+
+
+def sumar(n):
+    if n==0:
+        return 0
+    else:
+        return n+sumar(n-1)
+
+
+
+
+
